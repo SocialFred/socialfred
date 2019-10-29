3fd665aa3a20596dfa79896c69495eed1e36d8d8
Alexey Krasnoperov <alexey.krasnoperov@gmail.com>
2019-10-29 22:51:32 +0100

    frozen string literals, empty lines

--- lib/socialfred.rb
+++ lib/socialfred.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "socialfred/version"
 
 require 'socialfred/api'
--- spec/socialfred_spec.rb
+++ spec/socialfred_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Socialfred do
   it "has a version number" do
     expect(Socialfred::VERSION).not_to be nil
